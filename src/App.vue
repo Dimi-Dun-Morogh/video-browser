fdb84452784a47bc5f126358efd5454b7689aff9
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-12-11 12:15:04 +0200

    finish app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo"/>
+      <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
+import { YOUTUBE_API_KEY } from './config'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  data() {
+    return {
+      selectedVideo: {},
+      videos: [],
+    }
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: YOUTUBE_API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then(response => {
+          this.videos = response.data.items
+        })
+    },
+    onVideoSelect(video) {
+      this.selectedVideo = video
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import dotenv from 'dotenv'
 
-Vue.config.productionTip = false
+dotenv.config()
 
 new Vue({
   render: h => h(App),
